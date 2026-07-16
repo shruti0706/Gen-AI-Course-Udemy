ff794af1567837b6dd78d3dc52b534366b07e390
Shruti Agrawal <shruti762001@gmail.com>
2026-07-16 17:30:18 +0530

    imports

---
+++ python basics/5-Modules/package/__init__.py
@@ -0,0 +1,4 @@
+''' __init__.py is a special Python file that is used to mark a directory as a Python package. 
+It can be an empty file or contain initialization code for the package. 
+When a package is imported, the __init__.py file is executed, 
+allowing you to define package-level variables, functions, or classes. '''
---
+++ python basics/5-Modules/test.py
@@ -0,0 +1,6 @@
+from package.maths import *
+
+print(f"sum of 5 and 10 is {addition(5, 10)}")
+print(f"difference of 10 and 5 is {subtraction(10, 5)}")
+print(f"product of 5 and 10 is {multiplication(5, 10)}")
+print(f"quotient of 10 and 5 is {division(10, 5)}")
